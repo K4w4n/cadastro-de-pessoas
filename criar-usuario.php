58a67b8f8ed833c2c4af2d8d401b0e9924862cf4
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-01-12 17:32:38 -0300

    removendo bug de cor

--- criar-usuario.php
+++ criar-usuario.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style-criar-usuario.css">
     <link rel="shortcut icon" href="img/estudando.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0">
     <script src="javascript/script.js" defer></script>
 </head>
 
