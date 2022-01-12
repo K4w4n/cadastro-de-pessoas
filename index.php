898205af15724eb79f60eb2f463d013c3a62d2a6
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-01-11 23:29:38 -0300

    Tornando pagina de vizualização de pessoas responsiva

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Lista de pessoas</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="img/estudando.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0">
     <script src="javascript/script.js" defer></script>
 </head>
 
