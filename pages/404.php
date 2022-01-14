823116d1e8541c7da39a216c9144d1bb65415c47
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-01-14 17:33:10 -0300

    Customizando pagina 404

--- pages/404.php
+++ pages/404.php
@@ -1,2 +1,43 @@
-<?php
-echo 404;
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0">
+    <link rel="shortcut icon" href="img/estudando.png" type="image/x-icon">
+    <title>404 - Not Found</title>
+    <style>
+        * {
+            border: 0px;
+            margin: 0px;
+            padding: 0px;
+        }
+
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-family: sans-serif;
+        }
+
+        .image-404 {
+            width: 75%;
+        }
+    </style>
+</head>
+
+<body>
+    <img src="img/404.svg" alt="Homem com binóculo procurando algo" class="image-404">
+    <a href=".\">Ir para a página inicial</a>
+    <!-- <a href="https://storyset.com/online">Online illustrations by Storyset</a> -->
+</body>
+
+</html>
