af75b25f24203c8e3f24eab0e511a7156d116c3e
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-01-17 08:34:25 -0300

    Corrigindo bug do sumario

--- model/sumario.php
+++ model/sumario.php
@@ -2,9 +2,24 @@
 function sumarioPaginas($paginaAtual, $quantidadePaginas)
 {
     $paginaAtual++;
-
-    $indexInicial = ($paginaAtual <= 5) ? 1 : ($paginaAtual - 4);
-    $indexFinal = ($indexInicial + 9) <= $quantidadePaginas ? ($indexInicial + 9) : $quantidadePaginas;
+    if ($paginaAtual <= 5) {
+        $indexInicial = 1;
+        if ($quantidadePaginas < ($indexInicial + 9)) {
+            $indexFinal = $quantidadePaginas;
+        } else {
+            $indexFinal = $indexInicial + 9;
+        }
+    } elseif (($paginaAtual + 5) <= $quantidadePaginas) {
+        $indexInicial = $paginaAtual - 4;
+        $indexFinal = $paginaAtual + 5;
+    } else {
+        $indexFinal = $quantidadePaginas;
+        if (($indexFinal - 9) <= 0) {
+            $indexInicial = 1;
+        } else {
+            $indexInicial = $indexFinal - 9;
+        }
+    }
 
     $href = $paginaAtual == 1 ? '' : ' href = "?pg=' . ($paginaAtual - 1) . '"';
     echo ('<a class = "item-sumario seta"' . $href . '>&lt;</a>');
