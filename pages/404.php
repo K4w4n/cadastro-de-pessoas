c57f1ed0c09acce3b4b9c282dc520fa237521671
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-01-17 14:51:43 -0300

    Criando pagina 404

--- pages/404.php
+++ pages/404.php
@@ -6,38 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0">
     <link rel="shortcut icon" href="img/estudando.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/404.css">    
     <title>404 - Not Found</title>
-    <style>
-        * {
-            border: 0px;
-            margin: 0px;
-            padding: 0px;
-        }
-
-        html,
-        body {
-            width: 100%;
-            height: 100%;
-        }
-
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            font-family: sans-serif;
-        }
-
-        .image-404 {
-            width: 75%;
-        }
-    </style>
 </head>
 
 <body>
-    <img src="img/404.svg" alt="Homem com binóculo procurando algo" class="image-404">
-    <a href=".\">Ir para a página inicial</a>
-    <!-- <a href="https://storyset.com/online">Online illustrations by Storyset</a> -->
+    <p class="page-404">404</p>
+    <p class="not-found">Nos desculpe</p>
+    <p class="msg-not-found">Procuramos em todo lugar e não encontramos nada, volte a <a href=".\">página inicial</a> ou <a href="https://www.linkedin.com/in/kawan-ara%C3%BAjo-96ba001b1/">entre em contato com a gente</a>.</p>
 </body>
 
 </html>
